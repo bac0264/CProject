06b3d27a12055c18f840b7135d7b424c19b99221
unknown <hjg4shjn0akazu12@gmail.com>
2019-10-31 00:26:38 +0700

    setting

---
+++ Culprit/Assets/Scripts/Popup/SettingPopup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingPopup : BasePopup
+{
+    public Animator ani;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+    public void TurnOnSound()
+    {
+
+    }
+    public void TurnOffSound()
+    {
+
+    }
+    public void TurnOnMusic()
+    {
+
+    }
+    public void TurnOffMusic()
+    {
+
+    }
+    public void Language()
+    {
+
+    }
+}
