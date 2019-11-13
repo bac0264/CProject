2335e00b5cdf90e1ed5933734a33f16d49b2d411
Bac dzai <Bacnt>
2019-11-13 11:04:37 +0700

    commit

---
+++ Culprit/Assets/Scripts/GamePlay/SoundMode1.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundMode1 : MonoBehaviour
+{
+
+    public void BigCrash()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BigCrash();
+    }
+    public void BottleCrash()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleCrash();
+    }
+
+    public void BottleDrop()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleDrop();
+    }
+    public void BottleDrop1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleDrop1();
+    }
+    public void BottleFall()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleFall();
+    }
+    public void BottleFly()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleFly();
+    }
+    public void BottleHit()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleHit();
+    }
+    public void BottlePop()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottlePop();
+    }
+    public void BottleTing()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleTing();
+    }
+    public void BottleTing1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.BottleTing1();
+    }
+    public void Crash()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Crash();
+    }
+    public void Crash1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Crash1();
+    }
+    public void Fall()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fall();
+    }
+    public void Female_Angry()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Female_Angry();
+    }
+    public void Fidge_Open()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fidge_Open();
+    }
+    public void Fight_Hurt()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fight_Hurt();
+    }
+    public void Fight_Hurt2()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fight_Hurt2();
+    }
+    public void Fighting()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fighting();
+    }
+    public void Fighting1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fighting1();
+    }
+    public void Fist_Punch()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Fist_Punch();
+    }
+    public void Footstep1_room()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Footstep1_room();
+    }
+    public void Footstep_room()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Footstep_room();
+    }
+    public void Footstep_stone()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Footstep_stone();
+    }
+    public void Footstep_Road()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Footstep_Road();
+    }
+    public void Gasp()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Gasp();
+    }
+    public void Glass_bottle()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Glass_bottle();
+    }
+    public void Hit()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Hit();
+    }
+    public void Hiya()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Hiya();
+    }
+    public void Kick()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Kick();
+    }
+    public void Kick1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Kick1();
+    }
+    public void Kick2()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Kick2();
+    }
+    public void Laugh()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Laugh();
+    }
+    public void Laugh1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Laugh1();
+    }
+    public void MaleAngry()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.MaleAngry();
+    }
+    public void MaleAngry_1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.MaleAngry_1();
+    }
+    public void MaleFight()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.MaleFight();
+    }
+    public void MaleScream()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.MaleScream();
+    }
+    public void MaleScream1()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.MaleScream1();
+    }
+    public void Meow()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Meow();
+    }
+    public void Ough()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Ough();
+    }
+    public void Swoosh_slash()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Swoosh_slash();
+    }
+    public void Whoosch()
+    {
+        if (SoundManager.instance != null) SoundManager.instance.Whoosch();
+    }
+}
